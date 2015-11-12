1996583fc05f799dde159cfcc1cf54953c5de6bf
Mattt Thompson <mattt.thompson@apple.com>
2015-11-12 10:03:32 -0800

    Initial Commit

---
+++ Sources/Fisher-Yates_Shuffle.swift
@@ -0,0 +1,40 @@
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright 2015 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
+
+#if os(Linux)
+import Glibc
+#else
+import Darwin.C
+#endif
+
+public extension CollectionType {
+    func shuffle() -> [Generator.Element] {
+        var array = Array(self)
+        array.shuffleInPlace()
+
+        return array
+    }
+}
+
+public extension MutableCollectionType where Index == Int {
+    mutating func shuffleInPlace() {
+        guard count > 1 else { return }
+
+        for i in 0..<count - 1 {
+#if os(Linux)
+            let j = Int(g_random_int_range(0, Int32(count - i))) + i
+#else
+            let j = Int(arc4random_uniform(UInt32(count - i))) + i
+#endif
+            guard i != j else { continue }
+            swap(&self[i], &self[j])
+        }
+    }
+}
