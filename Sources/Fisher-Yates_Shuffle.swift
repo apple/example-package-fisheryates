d17785c9f3157920533110f2e1d85a1a525a9602
Max Howell <max.howell@apple.com>
2016-04-29 15:01:26 -0700

    Support 2.2 and 3.0

--- Sources/Fisher-Yates_Shuffle.swift
+++ Sources/Fisher-Yates_Shuffle.swift
@@ -8,11 +8,17 @@
  See http://swift.org/CONTRIBUTORS.txt for Swift project authors
 */
 
-#if os(Linux)
-import Glibc
-#else
-import Darwin.C
+#if swift(>=2.2) && !swift(>=3.0)
+    public typealias Collection = CollectionType
+
+    public protocol MutableCollection: MutableCollectionType {
+        associatedtype IndexDistance
+    }
+    extension Array: MutableCollection {
+        public typealias IndexDistance = Int
+    }
 #endif
+
 
 public extension Collection {
     func shuffled() -> [Generator.Element] {
@@ -22,20 +28,13 @@ public extension Collection {
     }
 }
 
-private func random(max: Int) -> Int {
-#if os(Linux)
-    return Int(random() % max)
-#else
-    return Int(arc4random_uniform(UInt32(max)))
-#endif
-}
 
 public extension MutableCollection where Index == Int, IndexDistance == Int {
     mutating func shuffle() {
         guard count > 1 else { return }
 
         for i in 0..<count - 1 {
-            let j = random(max: count - i) + i
+            let j = random(count - i) + i
             guard i != j else { continue }
             swap(&self[i], &self[j])
         }
