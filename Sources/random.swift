a09dfa7798afacf6d684648f39d48673d916bb68
Natthan <contraultra@users.noreply.github.com>
2016-05-03 13:26:05 -0700

    changed access level for random to public
    every package that has this package as a dependency gets random implemented for them to use for free

--- Sources/random.swift
+++ Sources/random.swift
@@ -11,11 +11,11 @@
 #if os(OSX)
     import Darwin
 
-    let random: (Int) -> Int = { Int(arc4random_uniform(UInt32($0))) }
+    public let random: (Int) -> Int = { Int(arc4random_uniform(UInt32($0))) }
 #else
     import Glibc
 
-    let random: (Int) -> Int = {
+    public let random: (Int) -> Int = {
         while true {
             let x = Glibc.random() % $0
             let y = Glibc.random() % $0
