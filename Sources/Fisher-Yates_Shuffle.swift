625927fe7658dc67dd047bf9257ff1eaf419aec3
J Cheyo Jimenez <cheyo@masters3d.com>
2016-05-21 08:25:12 -0700

    update

--- Sources/Fisher-Yates_Shuffle.swift
+++ Sources/Fisher-Yates_Shuffle.swift
@@ -1,38 +1,40 @@
 /*
  This source file is part of the Swift.org open source project
-
- Copyright 2015 Apple Inc. and the Swift project authors
+ 
+ Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
-
+ 
  See http://swift.org/LICENSE.txt for license information
- See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+ See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 */
 
-#if os(Linux)
-import Glibc
-#else
-import Darwin.C
+#if swift(>=2.2) && !swift(>=3.0)
+    public typealias Collection = CollectionType
+
+    public protocol MutableCollection: MutableCollectionType {
+        associatedtype IndexDistance
+    }
+    extension Array: MutableCollection {
+        public typealias IndexDistance = Int
+    }
 #endif
+
 
 public extension Collection {
     func shuffled() -> [Generator.Element] {
         var array = Array(self)
         array.shuffle()
-
         return array
     }
 }
 
-public extension MutableCollection where Index == Int {
+
+public extension MutableCollection where Index == Int, IndexDistance == Int {
     mutating func shuffle() {
         guard count > 1 else { return }
 
         for i in 0..<count - 1 {
-#if os(Linux)
-            let j = Int(random() % (count - i)) + i
-#else
-            let j = Int(arc4random_uniform(UInt32(count - i))) + i
-#endif
+            let j = random(count - i) + i
             guard i != j else { continue }
             swap(&self[i], &self[j])
         }
---
+++ Sources/random.swift
@@ -0,0 +1,25 @@
+/*
+ This source file is part of the Swift.org open source project
+ 
+ Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+ 
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+*/
+
+#if os(OSX)
+    import Darwin
+
+    public let random: (Int) -> Int = { Int(arc4random_uniform(UInt32($0))) }
+#else
+    import Glibc
+
+    public let random: (Int) -> Int = {
+        while true {
+            let x = Glibc.random() % $0
+            let y = Glibc.random() % $0
+            guard x == y else { return x }
+        }
+    }
+#endif
