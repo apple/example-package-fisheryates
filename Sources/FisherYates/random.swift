8667af7b35ea3a34da1f67b226944bd10bafd87f
NevinBR <nevin.brackettrozinsky@gmail.com>
2017-12-18 00:11:07 -0500

    Fixed generic constraints
    We need “random” to be generic over SignedInteger (at a minimum) because that is the constraint on Collection.IndexDistance which is our use case in “shuffle”. However it would be strange if “random” did not to work for unsigned integers, so we allow those too.
    
    It is unfortunate that Swift 3.1 had a BinaryInteger protocol which SignedInteger did not conform to, since otherwise we could spell the typealias “BinaryInteger”.

--- Sources/FisherYates/random.swift
+++ Sources/FisherYates/random.swift
@@ -9,21 +9,23 @@
 */
 
 #if swift(<=3.0)
-    typealias SignedInteger = _SignedIntegerType
-#elseif swift(<=4.0)
-    typealias SignedInteger = _SignedInteger
+    public typealias RandomInteger = IntegerArithmeticType
+#elseif swift(<=3.1)
+    public typealias RandomInteger = IntegerArithmetic
+#else
+    public typealias RandomInteger = BinaryInteger
 #endif
 
 #if os(OSX)
     import Darwin
 
-    public let random<T: SignedInteger>: (T) -> T = {
+    public let random<T: RandomInteger>: (T) -> T = {
         numericCast(arc4random_uniform(numericCast($0)))
     }
 #else
     import Glibc
 
-    public let random<T: SignedInteger>: (T) -> T = {
+    public let random<T: RandomInteger>: (T) -> T = {
         let upperLimit = RAND_MAX - RAND_MAX % numericCast($0)
         while true {
             let x = Glibc.random()
