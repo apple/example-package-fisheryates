e890f0f6abd0956d415d3bee57a99cd1cb736f49
NevinBR <nevin.brackettrozinsky@gmail.com>
2017-12-17 22:40:52 -0500

    Made random() generic, and fixed modulo bias
    To accommodate the removal of the “IndexDistance == Int” constraint in shuffle(), the random() function is now generic over SignedInteger.
    
    Also, the Glibc version of random() should no longer exhibited modulo bias.

--- Sources/FisherYates/random.swift
+++ Sources/FisherYates/random.swift
@@ -8,18 +8,26 @@
  See http://swift.org/CONTRIBUTORS.txt for Swift project authors
 */
 
+#if swift(<=3.0)
+    typealias SignedInteger = _SignedIntegerType
+#elseif swift(<=4.0)
+    typealias SignedInteger = _SignedInteger
+#endif
+
 #if os(OSX)
     import Darwin
 
-    public let random: (Int) -> Int = { Int(arc4random_uniform(UInt32($0))) }
+    public let random<T: SignedInteger>: (T) -> T = {
+        numericCast(arc4random_uniform(numericCast($0)))
+    }
 #else
     import Glibc
 
-    public let random: (Int) -> Int = {
+    public let random<T: SignedInteger>: (T) -> T = {
+        let upperLimit = RAND_MAX - RAND_MAX % numericCast($0)
         while true {
-            let x = Glibc.random() % $0
-            let y = Glibc.random() % $0
-            guard x == y else { return x }
+            let x = Glibc.random()
+            if x < upperLimit { return numericCast(x) % $0 }
         }
     }
 #endif
